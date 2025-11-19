ea4c2539680e24aee7e98c2781eadfb157005ee7
subash-tom <subash.a2025cse@sece.ac.in>
2025-11-19 09:43:16 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDU4HGdDu3CH4-OM8HgKfHorCSou3OIYTw",
+    authDomain: "harish-84726.firebaseapp.com",
+    projectId: "harish-84726",
+    storageBucket: "harish-84726.firebasestorage.app",
+    messagingSenderId: "119373404680",
+    appId: "1:119373404680:web:b9ab42651b01bd13a5cfbd"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
